8dab7dc490434344c3e225a34dd2ac61a84dc2cf
zgyr <0112d759@gmail.com>
2021-02-10 15:22:46 +0600

    now it works

--- python/irc.py
+++ python/irc.py
@@ -1,10 +1,11 @@
+import threading
 import socket
 import time
+import sys
 
 class IRC:
     
     def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = 'irc.esper.net'
         self.port = 6667
         self.name = 'randomname987654'
@@ -16,13 +17,31 @@ class IRC:
         self.send_raw(f'PRIVMSG {target} {message}')
     
     def connect(self):
+        self.online = True
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.server, self.port))
-        time.sleep(1)
+        time.sleep(0.5)
+        self.recvThread = threading.Thread(target = self.response)
+        self.recvThread.start()
         self.send_raw(f'USER {self.name} . . :{self.name}')
         self.send_raw(f'NICK {self.name}')
     
+    def disconnect(self):
+        self.send_raw('QUIT')
+        self.online = False
+        time.sleep(0.5)
+        self.socket.shutdown()
+        self.recvThread.join()
+    
     def response(self):
-        time.sleep(1)
-        resp = self.socket.recv(2048).decode('UTF-8')
-        if resp.find('PING') != -1:
-            self.send_raw('PONG ' + resp.split()[1])
+        while self.online:
+            try:
+                time.sleep(0.2)
+                resp = self.socket.recv(512).decode('UTF-8')
+                if resp.find('PING') != -1:
+                    self.send_raw('PONG ' + resp.split()[1])
+                print(resp) # debug
+            except OSError:
+                self.connect()
+            else:
+                pass
